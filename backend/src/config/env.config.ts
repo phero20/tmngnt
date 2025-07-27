e35bfaacd20f9170adf4a8e34564b8d7cfa3518c
Feroz <mdferozahmed27156@gmail.com>
2025-07-27 20:55:18 +0530

    s

---
+++ backend/src/config/env.config.ts
@@ -0,0 +1,20 @@
+const env = {
+    DATABASE_URL: process.env.DATABASE_URL,
+    PORT: process.env.PORT || 3000,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+    REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
+    NODE_ENV: process.env.NODE_ENV || 'development',
+}
+
+
+
+
+export const envConfig = {
+    get: <K extends keyof typeof env>(key: K): NonNullable<typeof env[K]> => {
+        if (!env[key]) {
+            throw new Error(`Environment variable ${key} is not defined`);
+        }
+        return env[key] as NonNullable<typeof env[K]>;
+    }
+}
