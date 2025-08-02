6ae9054def726b48c554d4f2ae3a804ae06f3b71
Feroz <mdferozahmed27156@gmail.com>
2025-08-02 21:36:41 +0530

    check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from 'hono';
+import { globalErrorHandler } from './utils/errors/error-handler';
+import authRoutes from './routes/auth.routes';
+import { logger } from 'hono/logger';
 
 const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('hello');
-});
+app.use('*', logger());
+app.onError(globalErrorHandler);
 
-export default app;
+app.get('/health', (c) => c.json({ status: 'ok' }));
+
+app.route('/', authRoutes);
+
+app.get('/', (c) => c.text('Hotel Management API'));
+
+export default {
+  port: process.env.PORT || 3000,
+  fetch: app.fetch,
+  hostname: '0.0.0.0',
+};
