4f0344c0d6c72aa14814e6b3eb7e1f93ea8feb63
Feroz <mdferozahmed27156@gmail.com>
2026-02-09 22:37:17 +0530

    dbhsgc

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,13 @@
-import { Hono } from 'hono';
+import { OpenAPIHono } from '@hono/zod-openapi';
 import authRoutes from './auth.routes';
+import hotelRoutes from './hotels';
 
-const app = new Hono();
+import amenityRoutes from './amenities/amenity.routes';
+
+const app = new OpenAPIHono();
 
 app.route('/', authRoutes);
+app.route('/api/hotels', hotelRoutes);
+app.route('/api/amenities', amenityRoutes);
 
 export default app;
