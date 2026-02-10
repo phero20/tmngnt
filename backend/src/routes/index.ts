5f0769158b26c9c2c61d1cb937863f38ec4993db
Feroz <mdferozahmed27156@gmail.com>
2026-02-10 14:45:40 +0530

    updated backend/src/routes/index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,11 +3,13 @@ import authRoutes from './auth.routes';
 import hotelRoutes from './hotels';
 
 import amenityRoutes from './amenities/amenity.routes';
+import bookingRoutes from './bookings';
 
 const app = new OpenAPIHono();
 
 app.route('/', authRoutes);
 app.route('/api/hotels', hotelRoutes);
 app.route('/api/amenities', amenityRoutes);
+app.route('/api/bookings', bookingRoutes);
 
 export default app;
