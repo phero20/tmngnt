9ba089d92096559c49a4437bd3b0830960a037bb
Feroz <mdferozahmed27156@gmail.com>
2026-02-10 14:37:02 +0530

    nig

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,6 +1,6 @@
 import { Context, Next } from 'hono';
 import { auth } from '../lib/auth';
-import { UnauthorizedError } from '../utils/errors/http-error';
+import { UnauthorizedError, ForbiddenError } from '../utils/errors/http-error';
 import { AppBindings } from '../types/hono.types';
 
 export const authMiddleware = async (c: Context<AppBindings>, next: Next) => {
@@ -10,6 +10,10 @@ export const authMiddleware = async (c: Context<AppBindings>, next: Next) => {
     throw new UnauthorizedError(
       'You must be logged in to access this resource'
     );
+  }
+
+  if (session.user.banned) {
+    throw new ForbiddenError('Your account has been banned.');
   }
 
   c.set('user', session.user);
