4dbf718ec638f9cc0b621bef7e8bca52dce6724c
Feroz <mdferozahmed27156@gmail.com>
2026-02-12 21:59:13 +0530

    updated web/src/constants/assets.ts

---
+++ web/src/constants/assets.ts
@@ -0,0 +1,5 @@
+export const ASSETS = {
+  AUTH_BACKGROUND:
+    'https://images.unsplash.com/photo-1618773928121-c32242e63f39?q=80&w=2070&auto=format&fit=crop',
+  // Add other assets here later
+};
